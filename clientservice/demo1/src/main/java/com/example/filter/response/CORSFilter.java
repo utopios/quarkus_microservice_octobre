6905a94f27cf327fce120599b9d1af70f4a8afe1
ihab abadi <ihab@utopios.net>
2024-10-03 10:20:40 +0200

    ajout des cors origin dans la partie client de la solution TP 2

---
+++ clientservice/demo1/src/main/java/com/example/filter/response/CORSFilter.java
@@ -0,0 +1,17 @@
+package com.example.filter.response;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+
+import java.io.IOException;
+
+
+@Provider
+public class CORSFilter implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext containerRequestContext, ContainerResponseContext containerResponseContext) throws IOException {
+        containerResponseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+    }
+}
